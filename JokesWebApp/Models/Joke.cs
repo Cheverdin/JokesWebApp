307739ff307b40247380633363399485fa542033
Vlad Cheverdin <44262932+Cheverdin@users.noreply.github.com>
2022-11-08 23:32:00 +0300

    JokesWebApp
    Finish PR

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
